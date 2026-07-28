bf519d0a08e03918e0c5e17f329999648974d1aa
unknown <nada bouta>
2026-07-28 14:43:40 +0100

    feat(jour1): factories et seeders donnees de test

---
+++ database/factories/DepartementFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Departement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Departement>
+ */
+class DepartementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
